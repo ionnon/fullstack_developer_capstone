d3341cbbb7233ecaa0870ac87c18a7f65e277cbb
ionnon <ionnon@users.noreply.github.com>
2026-07-06 21:02:05 -0400

    Fix frontend routes for dealers pages

--- djangoproj/urls.py
+++ djangoproj/urls.py
@@ -1,28 +1,24 @@
-"""djangoproj URL Configuration
+"""djangoproj URL Configuration"""
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
-from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
-    path('', TemplateView.as_view(template_name="Home.html")),
-    path('about/', TemplateView.as_view(template_name="About.html")),
-    path('contact/', TemplateView.as_view(template_name="Contact.html")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+    path('', TemplateView.as_view(template_name="Home.html"), name='home'),
+    path('about/', TemplateView.as_view(template_name="About.html"), name='about'),
+    path('contact/', TemplateView.as_view(template_name="Contact.html"), name='contact'),
+
+    path('dealers/', TemplateView.as_view(template_name="Home.html"), name='dealers'),
+    path('dealer/<int:id>/', TemplateView.as_view(template_name="Home.html"), name='dealer_detail'),
+    path('postreview/<int:id>/', TemplateView.as_view(template_name="Home.html"), name='post_review'),
+    path('login/', TemplateView.as_view(template_name="Home.html"), name='login'),
+    path('register/', TemplateView.as_view(template_name="Home.html"), name='register'),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
